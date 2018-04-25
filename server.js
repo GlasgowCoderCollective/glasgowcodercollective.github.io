c54deb315ea19a7792f81412e9783c373091b54c
Craig Sweaton <csweaton@gmail.com>
2018-04-25 06:43:28 +0100

    Added middleware to remove X-PoweredBy header

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const server = require('express')();
 const path = require('path');
 
+const { removeHeaders } = require('./middleware/headers');
 const indexRoute = require('./routes/indexRoute');
 
 server.use(require('express').static(path.resolve(__dirname, 'public')));
-server.use('/', indexRoute);
+server.use('/', removeHeaders, indexRoute);
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log('Server started on port', PORT));

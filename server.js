84669a6ad1f32cf7a82b7a495df0af2b7d37308b
Craig Sweaton <csweaton@gmail.com>
2018-06-28 18:17:22 +0100

    Added boilerplate code for new index page design

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const path = require("path");
 
 const { removeHeaders } = require("./middleware/headers");
 const indexRoute = require("./routes/indexRoute");
+const indexCandidateRoute = require("./routes/indexCandidateRoute");
 
 server.use(require("express").static(path.resolve(__dirname, "public")));
 server.use("/", removeHeaders, indexRoute);
+server.use("/beta", indexCandidateRoute);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log("Server started on port", PORT));

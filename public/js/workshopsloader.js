315d3e478740f6978a3d3095f0f409563395da8b
Craig Sweaton <csweaton@gmail.com>
2018-06-30 17:01:36 +0100

    Added workshops section with dynamic data

---
+++ public/js/workshopsloader.js
@@ -0,0 +1,22 @@
+var workshopsContainer = document.querySelector('.grid');
+
+function GetWorkshops() {
+    fetch('/api/workshops')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(workshops) {
+        workshops.map(function(workshop) {
+            workshopsContainer.innerHTML += displayWorkshop(workshop);
+        });
+    })
+    .catch(function() {
+        console.error('Could not display workshops');
+    })
+}
+
+function displayWorkshop(workshop) {
+    return `<div class="workshop"><h4>${workshop.title}</h4><p>${workshop.description}</p><a href="/workshop?id=${workshop.id}">Click here</a></div>`;
+}
+
+GetWorkshops();

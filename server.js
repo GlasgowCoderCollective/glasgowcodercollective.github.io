bbe23dff73da15c0983f46f9ec8ae0327eec705c
Craig Sweaton <craig.sweaton@outlook.com>
2020-01-28 21:16:53 +0000

    Feature: Connect database to application

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const env = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 if (process.env.NODE_ENV !== 'production') {
   env.config();
 }
 
-// TODO: Connect the application to the database
-
-app.listen(process.env.PORT, () => console.log('GCC App has started'));
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to database');
+    app.listen(process.env.PORT, () => console.log('GCC App has started'));
+  })
+  .catch((mongooseError) => {
+    console.error(mongooseError.message);
+    return process.exit(1);
+  });

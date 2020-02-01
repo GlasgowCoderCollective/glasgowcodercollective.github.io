e70d0fab83e9e0ad92494d00577d8afb431c76fa
Craig Sweaton <craig.sweaton@outlook.com>
2020-02-01 13:35:35 +0000

    Feature: Add express sslify

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const app = require('express')();
+const enforce = require('express-sslify');
 const cors = require('cors');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
@@ -7,6 +8,11 @@ const path = require('path');
 const clientPath = path.resolve(__dirname, 'client');
 const publicPath = path.resolve(__dirname, 'public');
 const apiRouter = require('./routes/api');
+
+// Force https if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+}
 
 app.use(cors());
 app.use(helmet());

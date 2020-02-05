2bffbf87b42f4fb4224f4e58dcab83102d63c84b
Craig Sweaton <craig.sweaton@outlook.com>
2020-02-05 21:35:42 +0000

    Feature: Secure all talk endpoints

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,18 @@
+const User = require('../models/user');
+
 const authCheck = (req, res, next) => {
   if (!req.user) {
     return res.redirect('/');
   }
 
-  return next();
+  return User.findById(req.user._id)
+    .then((user) => {
+      if (!user) {
+        return res.redirect('/');
+      }
+
+      return next();
+    });
 };
 
 module.exports = {
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const { authCheck } = require('../middleware/auth');
 
 const authRouter = require('./auth');
 const talksRouter = require('./talks');
 
 router.use('/auth', authRouter);
-router.use('/talks', talksRouter);
+router.use('/talks', authCheck, talksRouter);
 
 module.exports = router;

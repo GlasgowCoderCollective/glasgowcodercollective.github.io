17afd00d8a164a7be8068d4689a7896fae028dd6
Craig Sweaton <craig.sweaton@outlook.com>
2020-05-05 19:13:24 +0100

    Fix: Remove resources section

--- routes/api.js
+++ routes/api.js
@@ -3,10 +3,8 @@ const { authCheck } = require('../middleware/auth');
 
 const authRouter = require('./auth');
 const talksRouter = require('./talks');
-const resourceRouter = require('./resources');
 
 router.use('/auth', authRouter);
 router.use('/talks', authCheck, talksRouter);
-router.use('/resources', authCheck, resourceRouter);
 
 module.exports = router;
